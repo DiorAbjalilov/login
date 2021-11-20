e651433509c5a326904f3eb808eab1e79fc2d8a5
DiorAbjalilov <abjalilovdiyor@gmai.com>
2021-11-20 22:26:02 +0500

    sdff

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-let btn = document.querySelector("button");
-let arr = [];
-
-btn.addEventListener("click", (e) => {
+function sigin(e) {
   e.preventDefault();
   let user_name = document.querySelector("#name").value;
+  let email = document.querySelector("#email").value;
   let password = document.querySelector("#password").value;
   let obj = {
     user: user_name,
+    email: email,
     pass: password,
   };
-  arr.push(obj);
-  localStorage.setItem("user_back", JSON.stringify(arr));
-  console.log();
-});
+  if (user_name && password) {
+    let json = JSON.stringify(obj);
+    localStorage.setItem("back", json);
+    console.log("quwildi");
+  } else {
+    console.log("tuldir");
+  }
+}
+function login(e) {
+  e.preventDefault();
+  let user_name = document.querySelector("#name").value;
+  let password = document.querySelector("#password").value;
+  let user = localStorage.getItem("back");
+  let data = JSON.parse(user);
+  if (user == null) {
+    console.log("buw");
+  } else if (user_name == data.user_name && password == data.password) {
+    console.log("login buldi");
+  }
+}
