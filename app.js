94bfb8cfa594abf56ea44b713f5ca038ca0fe94f
DiorAbjalilov <abjalilovdiyor@gmai.com>
2021-11-22 22:43:57 +0500

    sdf

--- app.js
+++ app.js
@@ -10,14 +10,20 @@ btn.addEventListener("click", (e) => {
     email: email,
     password: pass,
   });
+  let aa = JSON.parse(localStorage.getItem("formDate"));
+  (aa || []).map((item) => {
+    console.log(item);
+  });
   localStorage.setItem("formDate", JSON.stringify(form_date));
   dispDate();
+  console.log(form_date);
   e.preventDefault();
 });
 function dispDate() {
   let adddd = JSON.parse(localStorage.getItem("formDate"));
   if (localStorage.getItem("formDate")) {
     let table = document.querySelector("table");
+    table.innerHTML = "";
     adddd.forEach((data) => {
       table.innerHTML += `
         <tr>
